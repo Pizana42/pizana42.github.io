c5ae2002c0b09984ceae4c221d3bbc3181d8c0dd
Pizana <pizana@gmx.ch>
2026-08-16 01:55:26 +0200

    shorten introduction, mobile experience

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def handle_input(command):
         s["out"] += "\n" + f"Dein aktueller Highscore ist: {s['highscore']}"
         s["out"] += "\n" + f"Deine bisherigen Scores sind: {', '.join(s['scores'])}"
     elif command in ("h", "hilfe", "help"):
-        s["out"] += "\n" + "Verfügbare Abkürzungen:\n  r  =  \"reset\"\n  s  =  \"score\"\n  h  =  \"hilfe\"\n  c  =  \"cheat\"\nWarte was, es gibt cheats?"
+        s["out"] += "\n" + "Verfügbare Abkürzungen:\n  s  =  \"score\"\n  r  =  \"reset\"\n  h  =  \"hilfe\"\n  c  =  \"cheat\"\nWarte was, es gibt cheats?"
     elif command in ("c", "cheat"):
         s["out"] += "\n" + "Pssst... Willst du ne Acht kaufen?"
         s["tries"] = 101
@@ -58,12 +58,13 @@ def check_guess(guess):
 def apply_score():
     if s["highscore"] == None:
         s["out"] += "\n" + f"{s['tries']} ist dein erster Highscore. Kannst du ihn verbessern?"
+        s["highscore"] = s['tries']
     elif s["highscore"] > s["tries"]:
         s["out"] += "\n" + f"Damit hast du deinen alten Highscore von {s['highscore']} verbessert!"
+        s["highscore"] = s['tries']
     else:
         s["out"] += "\n" + f"Dein Highscore bleibt weiterhin {s['highscore']}."
     s["scores"].append(str(s["tries"]))
-    s["highscore"] = min(s["scores"])
     new_round()
 
 def send_output():
@@ -81,7 +82,7 @@ reset_game()
 
 @when("click", "#submit")
 def send_guess():
-    command = web.page["input"].value
+    command = web.page["input"].value.lower()
     if not command:
         return
 
